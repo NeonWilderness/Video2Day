239d76ca60c1028adbe0d995f4e4e81edfdbb7a4
NeonWilderness <neonwilderness@ymail.com>
2015-01-05 17:17:57 +0100

    Version 0.4 - code optimizations

--- video2day.js
+++ video2day.js
@@ -3,19 +3,22 @@
     $.fn.video2day = function(){
         var $videos = this,
             video2dayObj = {
-                version: "0.3",
+                version: "0.4",
                 defaults: {
-                    'addFlexVideoClass': true, // true=Adds class "flex-video" to surrounding DIV (Foundation 5: enables responsive video layout)
-                    'wualaSourceURL': "http://www.wuala.com", // Wuala Source URL
-                    'wualaDirectURL': "http://content.wuala.com/contents" // Wuala Direct URL
+                    'addFlexVideoClass': (typeof $.fn.foundation !== 'undefined'), // true=Adds class "flex-video" to surrounding DIV (Foundation 5: enables responsive video layout)
+                    'position': 'bottom' // target position of video: "top"=prepend=video at top, "bottom"=append=video at bottom
                 },
+                vidFrame: '<iframe width="{w}" height="{h}" src={p} frameborder="0" allowfullscreen></iframe>',
                 vidParam : {
-                    youtube: '<iframe width="{w}" height="{h}" src="http://www.youtube.com/embed/{v}" frameborder="0" allowfullscreen></iframe>',
-                    vimeo: '<iframe width="{w}" height="{h}" src="http://player.vimeo.com/video/{v}?byline=0&amp;portrait=0" frameborder="0"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
-                    vevo: '<iframe width="{w}" height="{h}" src="http://cache.vevo.com/m/html/embed.html?video={v}" frameborder="0" allowfullscreen></iframe>',
-                    bliptv: '<iframe width="{w}" height="{h}" src="http://blip.tv/play/{v}?p=1" frameborder="0" allowfullscreen></iframe>',
-                    dailymotion: '<iframe width="{w}" height="{h}" src="http://www.dailymotion.com/embed/video/{v}" frameborder="0" allowfullscreen></iframe>',
-                    putpat: '<iframe width="{w}" height="{h}" src="http://www.putpat.tv/iframe/videos/{v}" frameborder="0" allowfullscreen></iframe>'
+                    youtube:     '"http://www.youtube.com/embed/{v}"',
+                    vimeo:       '"http://player.vimeo.com/video/{v}?byline=0&portrait=0"',
+                    vevo:        '"http://cache.vevo.com/assets/html/embed.html?video={v}"',
+                    bliptv:      '"http://blip.tv/play/{v}?p=1"',
+                    dailymotion: '"http://www.dailymotion.com/embed/video/{v}"',
+                    putpat:      '"http://www.putpat.tv/iframe/videos/{v}"'
+                },
+                errmsg: function(content){
+                    return '<p class="message">'+content+'</p>';
                 },
                 init: function(useroptions){
 //----------------- Take over user options, if any
@@ -23,26 +26,26 @@
                         vidTypes = Object.keys(video2dayObj.vidParam).concat("other"),
                         self, html, width, height, vid, ratio, vidType, hasPoster, imageExt;
 //----------------- For each of the html5video-instances...
-                    $videos.each( function(){
+                    $videos.each( function(){ debugger;
 //--------------------- Establish default values for each video (may be overwritten by class settings)
                         self = $(this);
                         html = "";
-                        width = self.parents("div").width();
+                        width = self.parents().width();
                         ratio = 16/9;
                         vidType = "";
                         hasPoster = false;
                         imageExt = "jpg";
-//--------------------- Analyze class names: split class specs to array and process each single class
+//--------------------- Analyze class names: split class spec to array and process each single class
                         var classList = self.attr('class').toLowerCase().split(/\s+/);
-                        $.each( classList, function(index, item){
-                            switch(item.substr(0,6)){
+                        $.each( classList, function(){
+                            switch(this.substr(0,6)){
 //------------------------- User specified explicit width parameter
                             case "width-":
-                              width = parseInt(item.substr(6,item.length-6), 10);
+                              width = parseInt(this.substr(6,this.length-6), 10);
                               break;
 //------------------------- User specified explicit aspect ratio (e.g. 4:3 as 1.3333, 16:9 as 1.7777, Widescreen 2.4:1 as 2.4)
                             case "ratio-":
-                              ratio = parseFloat(item.substr(6,item.length-6));
+                              ratio = parseFloat(this.substr(6,this.length-6));
                               break;
 //------------------------- User wants a poster image to be displayed on the video before it starts playing
                             case "poster":
@@ -50,39 +53,39 @@
                               break;
 //------------------------- User specified poster image file type other than standard jpg
                             case "image-":
-                              imageExt = item.substr(6,item.length-6);
+                              imageExt = this.substr(6,this.length-6);
                               break;
                             default:
-                              if (vidTypes.indexOf(item)>=0){ vidType = item; }
+                              if (vidTypes.indexOf(this)>=0){ vidType = this; }
                             }
                         });
 //--------------------- Calculate height based on specified ratio (Default is 16/9)
                         height = Math.round(width/ratio);
-//--------------------- Get the video-ID (YouTube, Vimeo, Vevo, BlipTV) or the File Link
+//--------------------- Get the video-ID or the file url
                         vid = self.attr("id") || "";
 //--------------------- Did user correctly specify the video id?
                         if (vid.length===0){
 //------------------------- No, then prepare error text
-                            html = '<p class="message">Bitte die Video-ID bzw. -URL im Parameter "id" ergänzen!</p>';
+                            html = video2dayObj.errmsg('Bitte die Video-ID bzw. -URL im Parameter "id" ergänzen!');
 //--------------------- Did user correctly specify a valid video type?
                         } else  if (vidType.length===0){
 //------------------------- No, then prepare error text
-                            html = '<p class="message">Bitte '+vidTypes.join(", ")+' im class-Parameter "' + self.attr('class') + '" ergänzen!</p>';
+                            html = video2dayObj.errmsg('Bitte '+vidTypes.join(", ")+' im class-Parameter "' + self.attr('class') + '" ergänzen!');
                         } else if (vidType==="other"){
 //------------------------- Generate the HTML5 player action with a hosted cloud mp4 file (e.g. Wuala, Dropbox); apply Wuala direct URL conversion
-                              vid = vid.replace(options.wualaSourceURL, options.wualaDirectURL);
                               var poster = (hasPoster) ? ' poster="' + vid.substr(0,vid.lastIndexOf(".")+1) + imageExt + '"' : "";
                               html = '<video class="video-js vjs-default-skin" controls preload="auto" width="' + width + '" height="' + height + '" data-setup="{}"' + poster + '><source src="' + vid + '" type="video/mp4"></video>';
                         } else {
 //----------------------- Generate the YouTube/Vimeo/Vevo/BlipTV video embed code
-                          html = video2dayObj.vidParam[vidType].replace(/{w}/gi,width).replace(/{h}/gi,height).replace(/{v}/gi,vid);
+                            html = video2dayObj.vidFrame
+                                .replace(/{w}/gi, width)
+                                .replace(/{h}/gi, height)
+                                .replace(/{p}/gi, video2dayObj.vidParam[vidType].replace(/{v}/gi,vid));
                         }
 //--------------------- Add "flex-video" responsive class in case Foundation5 is used
-                        if (options.addFlexVideoClass){
-                            if (!self.hasClass("flex-video")){ self.addClass("flex-video"); }
-                        }
+                        if (options.addFlexVideoClass) self.addClass("flex-video");
 //--------------------- Push the iframe/object into the HTML (div)
-                        self.append(html);
+                        if (options.position.toLowerCase()==="top") self.prepend(html); else self.append(html);
                     });
 //----------------- Enable jQuery chaining
                     return $videos;
